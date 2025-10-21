5407c094f8d821b1af256584e84873035957fcc2
tdballesteros <tim.ballesteros74@yahoo.com>
2025-10-20 22:36:42 -0400

    shapefile updates + UCDP death data formatting

---
+++ 0X-UCDP_death_data_formatting.R
@@ -0,0 +1,112 @@
+
+### load libraries ---------------------------------------------------------------------------------
+library(sf)
+library(readxl)
+library(mapview)
+library(tidyverse)
+
+### load data --------------------------------------------------------------------------------------
+gede <- readxl::read_xlsx("/Users/timmyballesteros/Downloads/GEDEvent_v25_1.xlsx")
+
+# load formatted shapefiles
+source("0X-Format_shapefiles.R")
+
+### format data ------------------------------------------------------------------------------------
+gede_bih <- gede %>%
+  dplyr::filter(
+    country == "Bosnia-Herzegovina"
+    ) %>%
+  # drop unneeded columns
+  dplyr::select(-c(active_year, code_status, number_of_sources, source_article, source_office,
+                   source_date, source_headline, source_original, adm_1, adm_2, geom_wkt,
+                   priogrid_gid, country, country_id, region, gwnoa, gwnob))
+
+gede_bih_geocoded <- gede_bih %>%
+  dplyr::filter(
+    # filter out location precision of 5 (linear geographic feature) or 6 (country-level))
+    where_prec %in% c(1, 2)
+    ) %>%
+  # convert to an sf object
+  st_as_sf(
+    coords = c("longitude", "latitude"),
+    crs = 4326,
+    remove = FALSE
+    ) %>%
+  dplyr::mutate(
+    # calculate the length of each event and add one so date range is inclusive of endpoints
+    event_length = difftime(date_end, date_start, units = "days") + 1
+    )
+
+### calculate death summary statistics -------------------------------------------------------------
+# total deaths
+sum(gede_bih$best, na.rm = FALSE)
+
+# geocoded deaths
+sum(gede_bih_geocoded$best, na.rm = FALSE)
+
+# UCDP's count is ~2/3 of Costalli & Moro's count.
+
+### prewar municipality mapping --------------------------------------------------------------------
+# confirm CRSs match
+sf::st_crs(gede2)$proj4string
+sf::st_crs(bih_prewar_municipalities_shapefile)$proj4string
+
+# spatial join
+gede_municipality_prewar_assignment <- sf::st_join(
+  gede2,
+  bih_prewar_municipalities_shapefile,
+  join = st_within,
+  left = TRUE
+)
+
+# collapse by municipality
+gede_municipality_prewar_counts <- gede_municipality_prewar_assignment %>%
+  dplyr::group_by(municipality) %>%
+  dplyr::summarise(
+    count_of_events = n(),
+    best = sum(best, na.rm = TRUE),
+    high = sum(high, na.rm = TRUE),
+    low = sum(low, na.rm = TRUE)
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::arrange(municipality)
+
+# write formatted data
+write.csv(gede_municipality_prewar_counts,
+          "Formatted Data/ucdp_deaths_prewar_municipality_formatted.csv",
+          row.names = FALSE)
+
+### postwar municipality mapping -------------------------------------------------------------------
+bih_postwar_municipalities_shapefile_formatted <- bih_postwar_municipalities_shapefile_formatted %>%
+  sf::st_make_valid()
+
+# confirm CRSs match
+sf::st_crs(gede2)$proj4string
+sf::st_crs(bih_postwar_municipalities_shapefile_formatted)$proj4string
+
+# spatial join
+gede_municipality_postwar_assignment <- sf::st_join(
+  gede2,
+  bih_postwar_municipalities_shapefile_formatted,
+  join = st_within,
+  left = TRUE
+)
+
+# collapse by municipality
+gede_municipality_postwar_counts <- gede_municipality_postwar_assignment %>%
+  dplyr::group_by(NAME_3) %>%
+  dplyr::summarise(
+    count_of_events = n(),
+    best = sum(best, na.rm = TRUE),
+    high = sum(high, na.rm = TRUE),
+    low = sum(low, na.rm = TRUE)
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::rename(municipality = NAME_3) %>%
+  dplyr::arrange(municipality)
+
+# write formatted data
+write.csv(gede_municipality_postwar_counts,
+          "Formatted Data/ucdp_deaths_postwar_municipality_formatted.csv",
+          row.names = FALSE)
+
